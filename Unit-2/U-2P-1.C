5df3e9e363d15dd6f56725cb663c5104735e1f31
aaryan113829 <91818148+aaryan113829@users.noreply.github.com>
2021-10-20 14:34:24 +0530

    Update U-2P-1.C

--- Unit-2/U-2P-1.C
+++ Unit-2/U-2P-1.C
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main(){
+
+    int num_1, num_2;
+
+    printf("\nEnter Your First Number: ");
+    scanf("%d", &num_1);
+    printf("\nEnter Your Second Number: ");
+    scanf("%d", &num_2);
+
+    if (num_1>num_2){
+
+        printf("\nYour Maximum Number is: %d", num_1);
+        printf("\nYour Minimum Number is: %d", num_2);
+    }
+    else if(num_2>num_1){
+
+        printf("\nYour Maximum Number is: %d", num_2);
+        printf("\nYour Minimum Number is: %d", num_1);
+
+    }
+    else {
+        printf("\nBoth Number Are Same: %d %d", num_1, num_2);
+    }
+
+    return 0;
+}
