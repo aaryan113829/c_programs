aa0ca7c7b8cdd143d7d00e2753e55ff8a6805f77
aaryan113829 <91818148+aaryan113829@users.noreply.github.com>
2021-10-20 18:14:19 +0530

    Create U-2P-6.c

---
+++ Unit-2/U-2P-6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int year;
+
+    printf("\nEnter Your First Number: ");
+    scanf("%d", &year);
+    
+    if (year % 400 == 0){
+        printf("\n%d is a leap year", year);
+    }
+    else if(year % 100 == 0){
+        printf("\n%d is not a leap year", year);
+
+    }
+    else if(year % 4 == 0){
+        printf("\n%d is a leap year", year);
+
+    }
+    else {
+        printf("\n%d is not a leap year", year);
+    }
+    
+    return 0;
+}
